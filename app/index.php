c185c88274d07f77acc504db57c41e98e890a23e
Przemysław Gawłowski <gawlowski.przemyslaw@protonmail.com>
2020-11-10 13:00:49 +0100

    Create DirectoryScanner class
    2020-10-11

--- app/index.php
+++ app/index.php
@@ -1,37 +1,9 @@
 <?php
 
+require "src/DirectoryScanner.php";
+use app\src\DirectoryScanner;
+
 echo "PHP Unzipper";
-
-class DirectoryScanner
-{
-    protected $dirPath;
-
-    public function __construct(string $filePath)
-    {
-        $this->dirPath = dirname($filePath);
-    }
-
-    public function getDirPath(): string
-    {
-        return $this->dirPath;
-    }
-
-    public function scanDir(): array
-    {
-        $files = scandir($this->dirPath);
-        $output = [];
-
-        foreach ($files as $file) {
-            if (in_array($file, ['.', '..'])) {
-                continue;
-            }
-
-            $output[] = $file;
-        }
-
-        return $output;
-    }
-}
 
 $directory = new DirectoryScanner(__FILE__);
 $files1 = $directory->scanDir();
---
+++ app/src/DirectoryScanner.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace app\src;
+
+class DirectoryScanner
+{
+    const TYPE_DIRS = 'dirs';
+    const TYPE_FILES = 'files';
+
+    protected $dirPath;
+
+    public function __construct(string $filePath)
+    {
+        $this->dirPath = dirname($filePath);
+    }
+
+    public function getDirPath(): string
+    {
+        return $this->dirPath;
+    }
+
+    public function scanDir(): array
+    {
+        $items = scandir($this->dirPath);
+        $output = [];
+
+        foreach ($items as $item) {
+            if (in_array($item, ['.', '..'])) {
+                continue;
+            }
+
+            $type = is_dir("{$this->dirPath}/{$item}") ? self::TYPE_DIRS : self::TYPE_FILES;
+
+            $output[$type][] = $item;
+            ksort($output[$type]);
+        }
+
+        return $output;
+    }
+}
