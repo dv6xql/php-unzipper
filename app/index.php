862ac9581c2739f05f9ac38591fce377d09e63c4
Przemysław Gawłowski <gawlowski.przemyslaw@protonmail.com>
2020-11-10 12:50:14 +0100

    Create DirectoryScanner
    2020-11-10

--- app/index.php
+++ app/index.php
@@ -1,3 +1,38 @@
 <?php
 
 echo "PHP Unzipper";
+
+class DirectoryScanner
+{
+    protected $dirPath;
+
+    public function __construct(string $filePath)
+    {
+        $this->dirPath = dirname($filePath);
+    }
+
+    public function getDirPath(): string
+    {
+        return $this->dirPath;
+    }
+
+    public function scanDir(): array
+    {
+        $files = scandir($this->dirPath);
+        $output = [];
+
+        foreach ($files as $file) {
+            if (in_array($file, ['.', '..'])) {
+                continue;
+            }
+
+            $output[] = $file;
+        }
+
+        return $output;
+    }
+}
+
+$directory = new DirectoryScanner(__FILE__);
+$files1 = $directory->scanDir();
+print_r($files1);
