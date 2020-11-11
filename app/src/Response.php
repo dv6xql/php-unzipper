a3ec46fd1f12976f053fd3887f0624d00d1245d4
Przemysław Gawłowski <gawlowski.przemyslaw@protonmail.com>
2020-11-11 13:48:11 +0100

    Render response
    2020-11-11

--- app/src/Response.php
+++ app/src/Response.php
@@ -4,20 +4,25 @@ namespace app\src;
 
 class Response
 {
-    public static function success(string $message, ?array $data = null): string
+    public static function success(string $message, ?array $data = null): array
     {
-        return json_encode([
+        return [
             'success' => 1,
             'message' => $message,
             'data' => $data
-        ]);
+        ];
     }
 
-    public static function error(string $message): string
+    public static function error(string $message): array
     {
-        return json_encode([
+        return [
             'success' => 0,
             'message' => $message,
-        ]);
+        ];
+    }
+
+    public static function render(array $response): void
+    {
+        echo json_encode($response);
     }
 }
